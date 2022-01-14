980a7a9f82d884a67781a8ee86a92cbdace4dc52
William Randall <15082470+randallwc@users.noreply.github.com>
2022-01-13 19:09:47 -0600

    create client and server

---
+++ lab2/clientTest.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+import socket
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('0.0.0.0', 8080))
+client.send("I am CLIENT\n".encode())
+from_server = client.recv(4096)
+client.close()
+print(from_server.decode())
