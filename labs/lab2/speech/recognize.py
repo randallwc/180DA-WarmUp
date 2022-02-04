becf8efb6243008e7a77bac43dd5fdf629e49162
William Randall <15082470+randallwc@users.noreply.github.com>
2022-02-04 05:27:27 -0600

    lab2 fin

---
+++ labs/lab2/speech/recognize.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print('listening ... ',end='',flush=True)
+    audio = r.listen(source)
+    print('done listening')
+try:
+    print("google recognizes:\n" + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("google unknown value error")
+except sr.RequestError as e:
+    print(f'error: {e}')
